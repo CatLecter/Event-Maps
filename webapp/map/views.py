dd4d083b5a26ed849ac708734b39a5662ef4b382
Vasilii Safronov <vasi1988@mail.ru>
2021-09-08 00:41:38 +0300

    added events filter

--- webapp/map/views.py
+++ webapp/map/views.py
@@ -21,15 +21,13 @@ def index():
     title = "EVENTS"
     coordinate = fetch_coordinates(current_user.address)
 
+    # тут нужно исправить запрос на выборку рекомендуемых событий (по тегам)
     created_events = Event.query.filter_by(creator_login=current_user.login).all()
-    # created_events = Event.query.order_by(Event.id).all()
 
-    # create_ballon_json(created_events)
-
+    create_ballon_json(created_events)
     user_events = []
     for event in created_events:
         user_events.append(f"{event.start_date} - {event.header}")
-
     return render_template(
         "map/ymaps.html",
         page_title=title,
@@ -41,30 +39,51 @@ def index():
     )
 
 
-@blueprint.route("/process-choice-all", methods=["POST"])
-def process_choice_all():
-    form = ChoiceAllForm()
-    if form.validate_on_submit():
-        all_events = Event.query.order_by(Event.id).all()
-        create_ballon_json(all_events)
-    return redirect(url_for("map.index"))
+@blueprint.route("/all_events")
+def all_events():
+    if not current_user.is_authenticated:
+        return redirect(url_for("user.login"))
+    title = "EVENTS"
+    coordinate = fetch_coordinates(current_user.address)
+
+    # запрос всех событий из базы
+    created_events = Event.query.order_by(Event.id).all()
+
+    create_ballon_json(created_events)
+    user_events = []
+    for event in created_events:
+        user_events.append(f"{event.start_date} - {event.header}")
+    return render_template(
+        "map/ymaps.html",
+        page_title=title,
+        zoom=16,
+        lat=coordinate[1],
+        long=coordinate[0],
+        data=url_for("static", filename="data.json"),
+        my_events=user_events,
+    )
 
 
-@blueprint.route("/process-choice-recommended", methods=["POST"])
-def process_choice_recommended():
-    form = ChoiceRecommendedForm()
-    if form.validate_on_submit():
-        recommended_events = Event.query.filter_by(
-            creator_login=current_user.login
-        ).all()
-        create_ballon_json(recommended_events)
-    return redirect(url_for("map.index"))
+@blueprint.route("/my_events")
+def my_events():
+    if not current_user.is_authenticated:
+        return redirect(url_for("user.login"))
+    title = "EVENTS"
+    coordinate = fetch_coordinates(current_user.address)
 
+    # запрос созданных мной событий из базы
+    created_events = Event.query.filter_by(creator_login=current_user.login).all()
 
-@blueprint.route("/process-choice-created", methods=["POST"])
-def process_choice_created():
-    form = ChoiceCreatedForm()
-    if form.validate_on_submit():
-        created_events = Event.query.filter_by(creator_login=current_user.login).all()
-        create_ballon_json(created_events)
-    return redirect(url_for("map.index"))
+    create_ballon_json(created_events)
+    user_events = []
+    for event in created_events:
+        user_events.append(f"{event.start_date} - {event.header}")
+    return render_template(
+        "map/ymaps.html",
+        page_title=title,
+        zoom=16,
+        lat=coordinate[1],
+        long=coordinate[0],
+        data=url_for("static", filename="data.json"),
+        my_events=user_events,
+    )
